79c2ef3e32c9ccd6c0ecc29782707a007ac7fc79
maagwiin <magnuwindell@gmail.com>
2020-03-19 22:14:25 -0300

    att porta

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'viwes'))
+  .set('view wngine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
 client.on('ready', () => {
   client.user.setActivity('pedra em aluno de BSI');
   console.log('Bot ativo');
