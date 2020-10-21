7e6e5859ec84dbed12eeda3fadc07b9fed505c31
Magnu Windell <magnuwindell@gmail.com>
2020-10-21 10:00:59 -0300

    embed regras

--- commands/regras.js
+++ commands/regras.js
@@ -1,11 +1,12 @@
 const {MessageEmbed} = require("discord.js");
+const rulesch = member.guild.channels.cache.find(ch => ch.name === 'regras');
 
 exports.run = (client, message, args) => {
     const embed = new MessageEmbed()
  
         .setTitle('REGRAS')
         .setColor(0xff0000)
-        .setDescription('Leia atentamente!');
+        .setDescription(`Leia todas as regras em ${rulesch}!`);
 
       message.channel.send(embed).catch(console.error);
 }
